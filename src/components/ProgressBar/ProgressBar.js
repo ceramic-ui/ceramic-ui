b34d721e61932995f46949a6b14c4744d7d288ae
George Haidar <ghaidar0@gmail.com>
2017-09-05 22:22:56 +1000

    add progress bar

---
+++ src/components/ProgressBar/ProgressBar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+export const appearances = {
+  primary: "brandPrimary",
+  secondary: "brandSecondary",
+  danger: "brandDanger",
+  success: "brandSuccess"
+};
+
+const percentage = ({ min, max, value }) =>
+  Math.max(Math.min(value / (max - min) * 100, 100), 0);
+
+const ProgressBar = styled.div.attrs({
+  role: "progressbar",
+  "aria-valuenow": ({ value }) => value,
+  "aria-valuemin": ({ min }) => min,
+  "aria-valuemax": ({ max }) => max
+})`
+  width: 100%;
+  height: ${props => props.theme.fontSize()};
+  margin-bottom: ${props => props.theme.spacingBase()};
+  background-color: ${props => props.theme.borderColor};
+  background-image: linear-gradient(
+    to right,
+    ${props => props.theme[appearances[props.appearance]]},
+    ${props => props.theme[appearances[props.appearance]]}
+  );
+  background-size: ${props => percentage(props)}% 100%;
+  background-repeat: no-repeat;
+  transition: background 300ms ease-in-out;
+`;
+ProgressBar.displayName = "ProgressBar";
+ProgressBar.propTypes = {
+  appearance: PropTypes.oneOf(Object.keys(appearances)).isRequired,
+  min: PropTypes.number.isRequired,
+  max: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired
+};
+ProgressBar.defaultProps = {
+  appearance: "primary",
+  min: 0,
+  max: 100
+};
+export default ProgressBar;
---
+++ src/components/ProgressBar/ProgressBar.stories.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
+import { number } from "@storybook/addon-knobs";
+
+import Container from "../Container";
+import ProgressBar from "./ProgressBar";
+
+storiesOf("ProgressBar", module).add(
+  "Basic",
+  withInfo()(() => {
+    const value = number("Value", 42);
+    return (
+      <Container>
+        <ProgressBar value={value} />
+        <ProgressBar appearance="secondary" value={value} />
+        <ProgressBar appearance="danger" value={value} />
+        <ProgressBar appearance="success" value={value} />
+      </Container>
+    );
+  })
+);
