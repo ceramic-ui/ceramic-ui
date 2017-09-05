b5c6409de5f6d042e329ce2bdb7b31fc42767474
George Haidar <ghaidar0@gmail.com>
2017-09-05 17:18:43 +1000

    add maxWidth media query mixin

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -2,13 +2,13 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-import media from "../../media";
+import { minWidth } from "../../media";
 
 const Container = styled.div`
   margin: 0 0 ${props => props.theme.spacingBase()};
   padding: 0 ${props => props.theme.paddingX()};
 
-  ${media.medium`max-width: ${props =>
+  ${minWidth.medium`max-width: ${props =>
     props.readable ? props.theme.readableWidth() : "auto"}`};
 `;
 Container.displayName = "Container";
--- src/media.js
+++ src/media.js
@@ -7,14 +7,20 @@ export const sizes = {
   xlarge: 1200
 };
 
-const media = Object.keys(sizes).reduce((acc, label) => {
+export const minWidth = Object.keys(sizes).reduce((acc, label) => {
   acc[label] = (...args) => css`
     @media (min-width: ${sizes[label]}px) {
       ${css(...args)};
     }
   `;
-
   return acc;
 }, {});
 
-export default media;
+export const maxWidth = Object.keys(sizes).reduce((acc, label) => {
+  acc[label] = (...args) => css`
+    @media (max-width: ${sizes[label] - 1}px) {
+      ${css(...args)};
+    }
+  `;
+  return acc;
+}, {});
