6ab9f2a9e24ad7fe39d04de96c9cfd6fed2d26f1
George Haidar <ghaidar0@gmail.com>
2017-09-07 15:21:25 +1000

    add Flex and Flex.Item components

---
+++ src/components/Flex/Flex.js
@@ -0,0 +1,55 @@
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+import { vrythm, vrythmPropTypes } from "../../mixins/spacing";
+
+export const Item = styled.div.attrs({
+  "data-flush": props => (props.flush ? true : undefined)
+})`
+  box-sizing: border-box;
+  flex-grow: ${props => props.grow || 0};
+  flex-shrink: ${props => props.shrink || 0};
+  flex-basis: ${props => props.basis || "auto"};
+  order: ${props => props.order || 0};
+`;
+Item.displayName = "Flex.Item";
+Item.propTypes = {
+  grow: PropTypes.number,
+  shrink: PropTypes.number,
+  order: PropTypes.number,
+  basis: PropTypes.string
+};
+
+const Flex = styled.div`
+  ${props => `
+    margin: 0 -${props.theme.spacingBase()};`};
+  display: ${props => (props.inline ? "inline-flex" : "flex")};
+  ${props => !!props.alignItems && `align-items: ${props.alignItems}`};
+  ${props => !!props.alignContent && `align-content: ${props.alignContent}`};
+  ${props =>
+    !!props.justifyContent && `justify-content: ${props.justifyContent}`};
+  ${props => !!props.wrap && `flex-wrap: ${props.wrap}`};
+  ${props => !!props.direction && `flex-direction: ${props.direction}`};
+
+  > ${Item} {
+    ${props => `
+      margin-left: ${props.theme.spacingBase()};
+      margin-right: ${props.theme.spacingBase()};`};
+  }
+`;
+
+Flex.displayName = "Flex";
+Flex.propTypes = {
+  inline: PropTypes.bool,
+  alignItems: PropTypes.string,
+  alignContent: PropTypes.string,
+  justifyContent: PropTypes.string,
+  wrap: PropTypes.string,
+  direction: PropTypes.string,
+  ...vrythmPropTypes
+};
+
+Flex.Item = Item;
+
+export default Flex;
---
+++ src/components/Flex/Flex.stories.js
@@ -0,0 +1,79 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { withInfo } from "@storybook/addon-info";
+import { select } from "@storybook/addon-knobs";
+
+import Container from "../Container";
+import P from "../Paragraph";
+import Flex from "./Flex";
+
+const itemAlignments = [
+  "normal",
+  "center",
+  "flex-start",
+  "flex-end",
+  "stretch",
+  "inherit",
+  "initial"
+];
+
+const contentAlignments = [
+  "center",
+  "flex-start",
+  "flex-end",
+  "stretch",
+  "inherit",
+  "initial"
+];
+
+storiesOf("Flex", module).add(
+  "Basic",
+  withInfo()(() => {
+    const alignItems = select("align-items", itemAlignments, "stretch");
+    const alignContent = select("align-content", contentAlignments, "unset");
+    const direction = select(
+      "flex-direction",
+      ["row", "row-reverse", "column", "column-reverse"],
+      "row"
+    );
+    const wrap = select(
+      "flex-wrap",
+      ["nowrap", "wrap", "wrap-reverse", "inherit", "initial"],
+      "wrap"
+    );
+    return (
+      <Container readable>
+        <Flex
+          wrap={wrap}
+          alignItems={alignItems}
+          alignContent={alignContent}
+          direction={direction}
+        >
+          <Flex.Item>
+            <P>
+              First example<br />Has two lines
+            </P>
+          </Flex.Item>
+          <Flex.Item flush>
+            <P>Second example</P>
+          </Flex.Item>
+          <Flex.Item>
+            <P>Third example</P>
+          </Flex.Item>
+          <Flex.Item order={-1}>
+            <P>Fourth example</P>
+          </Flex.Item>
+          <Flex.Item grow={1}>
+            <P>Fifth example</P>
+          </Flex.Item>
+          <Flex.Item>
+            <P>Sixth example</P>
+          </Flex.Item>
+          <Flex.Item>
+            <P>Seventh example</P>
+          </Flex.Item>
+        </Flex>
+      </Container>
+    );
+  })
+);
