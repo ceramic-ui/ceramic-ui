272766663df282746771d7faeeb4be1ffc889315
George Haidar <ghaidar0@gmail.com>
2017-09-08 11:13:45 +1000

    move media mixins to correct location

--- src/components/ButtonPanel/ButtonPanel.js
+++ src/components/ButtonPanel/ButtonPanel.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled, { css } from "styled-components";
 
-import { maxWidth, minWidth } from "../../media";
+import { maxWidth, minWidth } from "../../mixins/media";
 import { vrythm, vrythmPropTypes } from "../../mixins/spacing";
 
 export const justifyChoices = {
--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-import { minWidth } from "../../media";
+import { minWidth } from "../../mixins/media";
 import { vrythm, vrythmPropTypes } from "../../mixins/spacing";
 
 const Container = styled.div`
