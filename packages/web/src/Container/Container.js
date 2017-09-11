add1cba1aa21115f58a9f387922d1f926c22cb79
George Haidar <ghaidar0@gmail.com>
2017-09-11 16:26:07 +1000

    add option to disable gutters

--- packages/web/src/Container/Container.js
+++ packages/web/src/Container/Container.js
@@ -7,14 +7,15 @@ import { vrythm, vrythmPropTypes } from "../mixins/spacing";
 
 const Container = styled.div`
   ${props => vrythm};
-  padding: 0 ${props => props.theme.spacingBase()};
+  padding: 0 ${props => !props.disableGutter && props.theme.spacingBase()};
 
   ${minWidth.medium`max-width: ${props =>
-    props.readable ? props.theme.readableWidth() : "auto"}`};
+    props.readable ? props.theme.readableWidth() : "none"}`};
 `;
 Container.displayName = "Container";
 Container.propTypes = {
   readable: PropTypes.bool,
+  disableGutter: PropTypes.bool,
   ...vrythmPropTypes
 };
 
