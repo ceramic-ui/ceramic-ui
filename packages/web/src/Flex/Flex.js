6c3b9e978e56ce07f48b1dc59cc6b96e54467ca4
George Haidar <ghaidar0@gmail.com>
2017-09-11 16:26:59 +1000

    avoid padding items in column directions

--- packages/web/src/Flex/Flex.js
+++ packages/web/src/Flex/Flex.js
@@ -4,10 +4,12 @@ import styled from "styled-components";
 
 import { vrythm, vrythmPropTypes } from "../mixins/spacing";
 
+const columnDirections = ["column", "column-reverse"];
+const isVertical = props => columnDirections.indexOf(props.direction) >= 0;
+
 export const Item = styled.div.attrs({
   "data-flush": props => (props.flush ? true : undefined)
 })`
-  box-sizing: border-box;
   flex-grow: ${props => props.grow || 0};
   flex-shrink: ${props => props.shrink || 0};
   flex-basis: ${props => props.basis || "auto"};
@@ -23,19 +25,16 @@ Item.propTypes = {
 
 const Flex = styled.div`
   ${props => `
-    margin: 0 -${props.theme.spacingBase()};`};
-  display: ${props => (props.inline ? "inline-flex" : "flex")};
-  ${props => !!props.alignItems && `align-items: ${props.alignItems}`};
-  ${props => !!props.alignContent && `align-content: ${props.alignContent}`};
-  ${props =>
-    !!props.justifyContent && `justify-content: ${props.justifyContent}`};
-  ${props => !!props.wrap && `flex-wrap: ${props.wrap}`};
-  ${props => !!props.direction && `flex-direction: ${props.direction}`};
-
+    margin-left: ${props.theme.spacingBase(s => -1 * s)};
+    display: ${props.inline ? "inline-flex" : "flex"};
+    ${!!props.alignItems && `align-items: ${props.alignItems}`};
+    ${!!props.alignContent && `align-content: ${props.alignContent}`};
+    ${!!props.justifyContent && `justify-content: ${props.justifyContent}`};
+    ${!!props.wrap && `flex-wrap: ${props.wrap}`};
+    ${!!props.direction && `flex-direction: ${props.direction}`};
+  `};
   > ${Item} {
-    ${props => `
-      margin-left: ${props.theme.spacingBase()};
-      margin-right: ${props.theme.spacingBase()};`};
+    padding-left: ${props => props.theme.spacingBase()};
   }
 `;
 
