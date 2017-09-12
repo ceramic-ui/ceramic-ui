182ef444df970280289a75b83073807d4b17130f
George Haidar <ghaidar0@gmail.com>
2017-09-13 08:56:38 +1000

    wrap legend in div to prevent it from sitting over fieldset border

--- packages/web/src/Legend/Legend.js
+++ packages/web/src/Legend/Legend.js
@@ -1,12 +1,25 @@
 import React from "react";
-import PropTypes from "prop-types";
 import styled from "styled-components";
 
 import Label from "../Label";
 import { Heading4 } from "../Heading";
 
-const Legend = Heading4.withComponent("legend");
+// Wrapping <legend> with a <div> stops the default positioning of <legend>s
+// over the border of <fieldset>s
+const WrappedLegend = props => (
+  <div>
+    <legend {...props} />
+  </div>
+);
+
+const Legend = Heading4.withComponent(WrappedLegend).extend`
+  display: flex;
+  margin-top: 0;
+  margin-left: 0;
+  margin-right: 0;
+  padding: 0;
+  appearance: none;
+`;
 Legend.displayName = "Legend";
-Legend.propTypes = {};
 
 export default Legend;
