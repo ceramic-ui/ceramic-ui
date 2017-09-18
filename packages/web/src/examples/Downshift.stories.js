199d005ac7fe1c20bcaeeb4b08173d351cd1578f
George Haidar <ghaidar0@gmail.com>
2017-09-18 16:50:10 +1000

    simplify downshift demo by extracting some render logic into functions

--- packages/web/src/examples/Downshift.stories.js
+++ packages/web/src/examples/Downshift.stories.js
@@ -4,64 +4,75 @@ import { withInfo } from "@storybook/addon-info";
 import { action } from "@storybook/addon-actions";
 import Downshift from "downshift";
 
+import { darken } from "../colors";
 import Button from "../Button";
 import Card from "../Card";
+import Container from "../Container";
 import Input from "../Input";
-
-const items = ["apple", "orange", "carrot"];
 
 const DownButton = Button.extend`
   text-align: left;
   text-transform: none;
+  &[data-focused] {
+    background-color: ${props => darken(props.theme.white, 5)};
+    border-color: ${props => darken(props.theme.white, 5)};
+  }
 `;
 
 const DownCard = Card.extend`
+  ${props =>
+    props.isOpen &&
+    `
   border-top-left-radius: 0;
   border-top-right-radius: 0;
+  `};
 `;
 
 const DownInput = Input.extend`
-  border-bottom-left-radius: 0;
-  border-bottom-right-radius: 0;
+  ${props =>
+    props.isOpen &&
+    `
+    border-bottom-left-radius: 0;
+    border-bottom-right-radius: 0;
+    `};
 `;
 
-storiesOf("Examples", module).add(
-  "Using with downshift",
-  withInfo()(() => (
-    <Downshift onChange={action("onChange")}>
-      {({
-        getInputProps,
-        getItemProps,
-        isOpen,
-        inputValue,
-        selectedItem,
-        highlightedIndex
-      }) => (
-        <div>
-          <DownInput {...getInputProps({ placeholder: "Favorite color ?" })} />
-          {isOpen ? (
-            <DownCard>
-              {items
-                .filter(
-                  i =>
-                    !inputValue ||
-                    i.toLowerCase().includes(inputValue.toLowerCase())
-                )
-                .map((item, index) => (
-                  <DownButton
-                    block
-                    flush
-                    appearance="dark"
-                    {...getItemProps({ item })}
-                    key={item}
-                  >
-                    {item}
-                  </DownButton>
-                ))}
+const items = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+const filterItems = term =>
+  items.filter(item => item.toLowerCase().indexOf(term.toLowerCase()) >= 0);
+
+const renderItems = (items, { getItemProps, highlightedIndex }) => {
+  return items.map((item, index) => (
+    <DownButton
+      block
+      flush
+      data-focused={highlightedIndex === index || undefined}
+      appearance="dark"
+      key={item}
+      {...getItemProps({ item })}
+    >
+      {item}
+    </DownButton>
+  ));
+};
+
+storiesOf("Examples", module).add("Using with downshift", () => (
+  <Downshift onChange={action("onChange")}>
+    {childProps => {
+      const { getRootProps, getInputProps, isOpen, inputValue } = childProps;
+      return (
+        <Container readable {...getRootProps({ refKey: "innerRef" })}>
+          <DownInput
+            {...getInputProps({ placeholder: "Favorite color?" })}
+            isOpen={isOpen}
+          />
+          {isOpen && (
+            <DownCard isOpen>
+              {renderItems(filterItems(inputValue), childProps)}
             </DownCard>
-          ) : null}
-        </div>
-      )}
-    </Downshift>
-  ))
-);
+          )}
+        </Container>
+      );
+    }}
+  </Downshift>
+));
