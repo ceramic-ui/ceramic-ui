f63f0530856a35efa4758e028ea431b9dff93797
George Haidar <ghaidar0@gmail.com>
2017-09-22 23:10:07 +1000

    make smarter Item spacing decisions based on flex direction and wrap

--- packages/web/src/Flex/Flex.js
+++ packages/web/src/Flex/Flex.js
@@ -1,11 +1,7 @@
 import PropTypes from "prop-types";
 import styled from "styled-components";
 
-import { spacingPropTypes } from "../mixins/spacing";
-
-export const Item = styled.div.attrs({
-  "data-flush": props => (props.flush ? true : undefined)
-})`
+export const Item = styled.div`
   flex-grow: ${props => props.grow || 0};
   flex-shrink: ${props => props.shrink || 0};
   flex-basis: ${props => props.basis || "auto"};
@@ -19,18 +15,27 @@ Item.propTypes = {
   basis: PropTypes.string
 };
 
+const isColumn = ({ direction }) =>
+  direction === "column" || direction === "column-reverse";
+const doesWrap = ({ wrap }) => wrap === "wrap" || wrap === "wrap-reverse";
+
 const Flex = styled.div`
   ${props => `
-    margin-left: ${props.theme.spacingBase(s => -1 * s)};
+    margin-bottom: ${doesWrap(props)
+      ? props.theme.spacingBase(s => -1 * s)
+      : 0};
+    margin-left: ${isColumn(props) ? 0 : props.theme.spacingBase(s => -1 * s)};
     display: ${props.inline ? "inline-flex" : "flex"};
     ${!!props.alignItems && `align-items: ${props.alignItems}`};
     ${!!props.alignContent && `align-content: ${props.alignContent}`};
     ${!!props.justifyContent && `justify-content: ${props.justifyContent}`};
     ${!!props.wrap && `flex-wrap: ${props.wrap}`};
     ${!!props.direction && `flex-direction: ${props.direction}`};
-  `};
+    `};
   > ${Item} {
-    padding-left: ${props => props.theme.spacingBase()};
+    padding-left: ${props => (isColumn(props) ? 0 : props.theme.spacingBase())};
+    padding-bottom: ${props =>
+      isColumn(props) || doesWrap(props) ? props.theme.spacingBase() : 0};
   }
 `;
 
@@ -41,8 +46,7 @@ Flex.propTypes = {
   alignContent: PropTypes.string,
   justifyContent: PropTypes.string,
   wrap: PropTypes.string,
-  direction: PropTypes.string,
-  ...spacingPropTypes
+  direction: PropTypes.string
 };
 
 Flex.Item = Item;
