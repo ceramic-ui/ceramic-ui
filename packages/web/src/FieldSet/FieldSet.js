e6d6d2c850d0404ec2d54f6dcd37bcb142c3e88a
George Haidar <ghaidar0@gmail.com>
2017-09-23 15:21:09 +1000

    reset fieldset margins

--- packages/web/src/FieldSet/FieldSet.js
+++ packages/web/src/FieldSet/FieldSet.js
@@ -1,6 +1,8 @@
 import Box from "../Box";
 
 const FieldSet = Box.withComponent("fieldset").extend`
+  margin-left: 0;
+  margin-right: 0;
   ${props =>
     `
       border: ${props.theme.borderWidth()} solid ${props.theme.borderColor};
