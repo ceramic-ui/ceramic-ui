1cd3e222aa7e6fe17e67ab9ca51a7f11a87b15e1
George Haidar <ghaidar0@gmail.com>
2017-09-23 14:35:22 +1000

    add ThemeProvider component that uses default theme as base for others

--- packages/web/.storybook/config.js
+++ packages/web/.storybook/config.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { addDecorator, configure } from "@storybook/react";
-import styled, { ThemeProvider } from "styled-components";
+import styled from "styled-components";
 import { withKnobs, select } from "@storybook/addon-knobs";
 
 import reset from "../src/reset";
 import themes from "../src/themes";
 import SystemFontStack from "../src/SystemFontStack";
+import ThemeProvider from "../src/ThemeProvider";
 
 const req = require.context("../src", true, /.stories.js$/);
 
