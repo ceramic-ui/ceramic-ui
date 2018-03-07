0507d41e92ce52b3c13b89746d1cb56a22446b7c
George Haidar <ghaidar0@gmail.com>
2018-03-07 23:11:45 +0000

    moved looky to a separate repo

--- packages/looky/src/dlv.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "dlv" {
-  interface Dlv {
-    <T>(o: T, k: string | string[]): any;
-    <T, V>(o: T, k: string | string[], d: V): V;
-  }
-  const dlv: Dlv;
-  export default dlv;
-}
